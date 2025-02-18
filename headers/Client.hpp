2dd479f5d6696f2754239abf2d2d71212ff887b6
Alexie Julien <aljulien@z2r12p2.42lyon.fr>
2025-02-18 17:15:42 +0100

    add: Added .hpp/.cpp for Server and Client classes
    Added all the private attributes and 3 methods to be coded in the Server class. The class Client is raw.
    Remove a.out execute
    Added snippet for canonical classes

---
+++ headers/Client.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aljulien <aljulien@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 16:28:43 by aljulien          #+#    #+#             */
+/*   Updated: 2025/02/18 16:28:44 by aljulien         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLIENT_HPP
+# define CLIENT_HPP
+
+class Client
+{
+public:
+	/*                               ORTHODOX CLASS                           */
+	Client(void);
+	Client(const Client &rhs);
+	~Client(void);
+	Client &operator=(const Client &rhs);
+
+	/*                               METHODS                                  */
+	
+	/*                               GETTERS                                  */
+	
+	/*                               SETTERS                                  */
+private:
+	
+};
+
+#endif
--- headers/Server.hpp
+++ headers/Server.hpp
@@ -6,16 +6,32 @@
 /*   By: aljulien <aljulien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 15:25:50 by aljulien          #+#    #+#             */
-/*   Updated: 2025/02/18 15:25:52 by aljulien         ###   ########.fr       */
+/*   Updated: 2025/02/18 17:14:04 by aljulien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_HPP
-# define SERVER_HPP
+#define SERVER_HPP
 
-class Server
-{
-public:
+#define MAX_EVENTS 100
+
+#include "Client.hpp"
+#include <iostream>
+#include <map>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <string>
+#include <sys/epoll.h>
+
+class Client;
+
+//TODO : see if we create map for channels
+//TODO : add queue in case maxevent is returned, for handling too many events at once
+//TODO : Encryption password ?
+//TODO : check if we need to use IPv6 adresses or IPv4 is good enough ?
+class Server {
+
+  public:
 	/*                               ORTHODOX CLASS                           */
 	Server(void);
 	Server(const Server &rhs);
@@ -23,12 +39,18 @@ public:
 	Server &operator=(const Server &rhs);
 
 	/*                               METHODS                                  */
-	
-	/*                               GETTERS                                  */
-	
-	/*                               SETTERS                                  */
-private:
-	
+	bool servInit();
+	bool servRun();
+	bool acceptClient();
+
+  private:
+	const std::string _password;
+	std::map< int, Client & > _client;
+	const int _port;
+	int _epollfd;
+	int _servFd;
+	struct epoll_event _servPoll;
+	struct sockaddr_in _servAddress;
 };
 
-#endif
+#endif //SERVER_HPP
