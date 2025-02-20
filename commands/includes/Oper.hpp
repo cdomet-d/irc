820b3c2e84a6d8362f8ce2cc99ee197e7266e9aa
cha202 <116841786+cha202@users.noreply.github.com>
2025-02-20 17:31:19 +0100

    add: header Oper.hpp with description comment

---
+++ commands/includes/Oper.hpp
@@ -0,0 +1,29 @@
+/*
+
+//requirement
+registration succeded
+
+//parsing
+just 2 params : <user> <password>
+
+//attributes
+command name : OPER
+
+//methods
+-- checkers --
+clientAuth() -> checks if the client has the right to become an operator \
+				if not display ERR_NOOPERHOST
+
+pwMatch() -> checks to see if the given password is correct \
+			in case of failure display ERR_PASSMISMATCH
+
+enoughParam() -> checks if there are the right nb of params \
+				in case of failure display ERR_NEEDMOREPARAMS
+
+-- setters --
+setOper() -> changes client status \
+				send concerned client RPL_YOUROPER message \
+				and a MODE message indicating his new user modes \
+				also inform channel of this new operator using MODE +o
+
+*/
--- commands/includes/Pass.hpp
+++ commands/includes/Pass.hpp
@@ -10,7 +10,7 @@ state : done or not (could be used to know if we can execute the command NICK)
 
 // methods :
 -- checkers -- :
-pwMatch() -> checks to see if the given password is the same as the server's \
+pwMatch() -> checks to see if the given password is correct \
 			in case of failure display ERR_PASSMISMATCH
 
 enoughParam() -> checks if there are the right nb of params \
