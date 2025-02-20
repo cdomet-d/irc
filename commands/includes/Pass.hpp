2538f74ffae3951d467862d7e34609423b4c04e9
cha202 <116841786+cha202@users.noreply.github.com>
2025-02-20 16:19:23 +0100

    add: header Pass.hpp with description comment

--- commands/includes/Pass.hpp
+++ commands/includes/Pass.hpp
@@ -4,6 +4,10 @@
 there must be only one param <password>
 can there be spaces in a password ??
 
+//attributes
+command name : PASS
+state : done or not (could be used to know if we can execute the command NICK)
+
 // methods :
 -- checkers -- :
 pwMatch() -> checks to see if the given password is the same as the server's \
@@ -13,11 +17,7 @@ enoughParam() -> checks if there are the right nb of params \
 				in case of failure display ERR_NEEDMOREPARAMS
 
 -- setters -- :
-setPw(std::string param) -> sets clients password to given string \
-							if already set display ERR_ALREADYREGISTERED
-
-//attributes
-state : done or not (could be used to know if we can execute the command NICK)
-
+setPw() -> sets clients password to given string \
+			if already set display ERR_ALREADYREGISTERED
 
 */
