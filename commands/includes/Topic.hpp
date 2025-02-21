3372d09eb953d5f18fcf3e18d86dc733fb322bb5
cha202 <116841786+cha202@users.noreply.github.com>
2025-02-21 15:45:04 +0100

    add: Topic.hpp header with descriptive comment

---
+++ commands/includes/Topic.hpp
@@ -0,0 +1,33 @@
+/*
+
+//requirement
+registration succeeded
+
+//parsing
+at least 1 param, there can be 2 : <channel> [<topic>]
+
+//methods
+-- checkers --
+enoughParam() -> checks if there are the right nb of params \
+				in case of failure display ERR_NEEDMOREPARAMS
+
+validChan() -> checks if channel exists if not display ERR_NOSUCHCHANNEL
+
+(if the client wants to view the topic of a channel but is not on it we can prohibit it. it's up to us)
+onChan() -> checks if the client belongs to the channel \
+			if not display ERR_NOTONCHANNEL
+
+hasChanPriv() -> checks if client has the appropriate channel privileges to execute command \
+					if not display ERR_CHANOPRIVSNEEDED
+
+-- executors --
+changeTopic() -> if topic (can be NULL) is given change the channel's topic \
+					use displayTopic() to send message to each client in the channel \
+					including the author, of the change. \
+					if the topic is changed to exacly the same as it was \
+					we can choose to notify the users or not
+
+displayTopic() -> display RPL_TOPIC followed by RPL_TOPICWHOTIME \
+					or RPL_NOTOPIC if channel lacks a topic
+
+*/
