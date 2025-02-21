ec49b8a754aea42104760b073834ee20a096ed1c
cha202 <116841786+cha202@users.noreply.github.com>
2025-02-21 14:02:50 +0100

    add: Part.hpp header with descriptive message

---
+++ commands/includes/Part.hpp
@@ -0,0 +1,23 @@
+/*
+
+//requirement
+registration succeeded
+
+//parsing
+at least 1 param
+params are seperated with a coma ","
+
+//attributes
+command name : PART
+
+//methods
+-- checkers --
+enoughParam() -> checks if there are the right nb of params \
+				in case of failure display ERR_NEEDMOREPARAMS
+
+partChanRequest() -> 
+
+-- setters --
+rmFromChan() -> removes client from channel
+
+*/
