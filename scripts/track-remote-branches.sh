04d94cb96cd99e95051ea735b48ffeed13b6dec5
Jauppp <coralie.dometdemont@gmail.com>
2025-02-24 19:47:57 +0100

    feat: improved git automation script

--- scripts/track-remote-branches.sh
+++ scripts/track-remote-branches.sh
@@ -1,14 +1,36 @@
+#!/bin/bash
+
 git fetch --all --prune
-git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -d
+
+if git branch -vv | grep ': gone]'; then
+	git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -d
+fi
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 git branch -r | grep -v '\->' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | while read remote; do
     local_branch="${remote#origin/}"
     if ! git branch --list "$local_branch" | grep -q "$local_branch"; then
         git branch --track "$local_branch" "$remote"
         echo "Created tracking branch: $local_branch"
-		git pull
     else
         echo "Tracking branch already exists: $local_branch"
-		git pull
     fi
+    
+	if [ $(git rev-list --count $local_branch -- origin/$local_branch) -gt 0 ]; then
+		if git switch "$local_branch"; then
+			git pull origin "$local_branch"
+		else
+			error="true"
+			break
+		fi
+	fi
 done
+
+if [ "$error" == "true" ]; then
+	echo "Please commit your current changes"
+fi
+
+if [ "$(git rev-parse --abbrev-ref HEAD)" != "$current_branch" ]; then
+	git switch "$current_branch"
+fi
