abc052dd4eb226b73737f83cb3cc26b3bbb52078
Charlotte Sweeting <charlottesweeting@outlook.fr>
2025-02-24 18:18:48 +0100

    fix: added comments

--- builder/includes/builder.hpp
+++ builder/includes/builder.hpp
@@ -6,7 +6,23 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 17:06:23 by csweetin          #+#    #+#             */
-/*   Updated: 2025/02/24 17:06:24 by csweetin         ###   ########.fr       */
+/*   Updated: 2025/02/24 18:18:26 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+
+ex input : "/join #channel"
+
+steps :
+- parsing
+
+- building
+	-permission
+	-argument
+		-checkers
+	-build
+
+- execute with director
+
+*/
