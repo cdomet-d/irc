e49850ae60bdf45d1dcc27df873990a3f7237937
Charlotte Sweeting <charlottesweeting@outlook.fr>
2025-02-24 16:50:12 +0100

    add: Mode.hpp header with descriptive comment

--- commands/includes/Mode.hpp
+++ commands/includes/Mode.hpp
@@ -12,8 +12,13 @@ command name : MODE
 -- checkers --
 validChan() -> checks if channel exists if not display ERR_NOSUCHCHANNEL
 
+hasChanPriv() -> checks if client has the appropriate channel privileges to execute command \
+					if not display ERR_CHANOPRIVSNEEDED
+
 -- executors --
-changeMode() -> sets or removes given mode
+changeMode() -> sets or removes given mode \
+					if <modestring> is not given display RPL_CHANNELMODEIS \
+					followed 
 
 
 */
