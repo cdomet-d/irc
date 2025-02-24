fc6b9e40609c02da804ef9956384415dc1f12508
cha202 <116841786+cha202@users.noreply.github.com>
2025-02-24 11:49:58 +0100

    fix: added precision to checker in Privmsg.hpp

--- commands/includes/Privmsg.hpp
+++ commands/includes/Privmsg.hpp
@@ -12,14 +12,21 @@ command name : PRIVMSG
 
 //methods
 -- checkers --
-validMess() -> if target is a channel check channel modes cause it can affect the message \
-				if it cannot be delivered to channel display ERR_CANNOTSENDTOCHAN
+validMess() -> check : \
+				- if there is a text to be sent, if not display ERR_NOTEXTTOSEND \
+				- if there is a target, if not display ERR_NORECIPIENT
+				if target is a channel : \
+					- check channel modes because they can affect the message \
+							if it cannot be delivered to channel display ERR_CANNOTSENDTOCHAN \
+			    else if target is a user : \
+					- check if nickname exists, if not display ERR_NOSUCHNICK \
+
 
 -- executors --
 sendMess() -> send <text to be sent> to target \
 				if target is a channel, check for a prefixe and \
 				send message only to clients with appropriate status \
 				else if it is a user, if user has been set as away display RPL_AWAY \ (not sure if we have to do this)
-				if the target starts with a $, the message is a broadcast
+				if the target starts with a $, the message is a broadcast (sent to all clients on server)
 
 */
