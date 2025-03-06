1b62b53fee9ec248cd5d250f4962f44fe9d21dbc
Alexie Julien <aljulien@student.42lyon.fr>
2025-03-06 11:58:42 +0100

    refactor : topic command
    now each use of the topic command has it's own function. The parsing is
    still basic but will be handle by the builder. Some features :
    We cannot see the topic of a channel if not part of it.
    Everyone can change the topic of a channel.
    Everyone can clear the topic of a channel
    We return the right replies for each 'error' a client might encounter

--- src/Topic.cpp
+++ src/Topic.cpp
@@ -6,13 +6,46 @@
 /*   By: aljulien <aljulien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 10:55:57 by aljulien          #+#    #+#             */
-/*   Updated: 2025/03/05 16:47:36 by aljulien         ###   ########.fr       */
+/*   Updated: 2025/03/06 11:58:20 by aljulien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 #include "Client.hpp"
 #include <sstream>
+
+void checkTopic(Channel *currentChannel, Client *whatCli, int fdClient) {
+	if (currentChannel->getTopic().empty() == true) {
+		log(DEBUG, "No topic found : ", RPL_NOTOPIC(whatCli->getNick(), currentChannel->getName()));
+		sendReply(fdClient, RPL_NOTOPIC(whatCli->getNick(), currentChannel->getName()));
+		return ;
+	}
+	log(DEBUG, "Topic found : ", RPL_TOPIC(whatCli->getNick(), currentChannel->getName(), currentChannel->getTopic()));
+	sendReply(fdClient, RPL_TOPIC(whatCli->getNick(), currentChannel->getName(), currentChannel->getTopic()));
+	return ;
+}
+
+void clearTopic(Channel *currentChannel, Client *whatCli) {
+	log(DEBUG, "-----clearTopic-----");
+	currentChannel->setTopic("");
+	for (std::map<int, Client *>::iterator it = currentChannel->getCliInChannel().begin();
+	it != currentChannel->getCliInChannel().end(); ++it) {
+		sendReply(it->second->getFd(), RPL_NOTOPIC(whatCli->getNick(), currentChannel->getName()));
+		log(DEBUG, "sending clear the topic lol");
+
+	}
+}
+
+void changeTopic(Channel *currentChannel, Client *whatCli, std::string topic) {
+	topic.erase(1, 0); //remove the ':'
+	currentChannel->getTopic().clear();
+	currentChannel->setTopic(topic);
+	log(DEBUG, "Topic set : ", RPL_TOPICCHANGED(whatCli->getPrefix(), currentChannel->getName(), currentChannel->getTopic()));
+
+	for (std::map<int, Client *>::iterator it = currentChannel->getCliInChannel().begin();
+		it != currentChannel->getCliInChannel().end(); ++it) {
+			sendReply(it->second->getFd(), RPL_TOPICCHANGED(whatCli->getPrefix(), currentChannel->getName(), currentChannel->getTopic())); }
+}
 
 bool handleTopic(std::string params, int fd) {
 	log(DEBUG, "-----handleTopic-----");
@@ -21,64 +54,37 @@ bool handleTopic(std::string params, int fd) {
 	
 	std::istringstream iss(params);
 	std::string channelName;
-	std::string topic;
-
+	std::string topic;	
 	iss >> channelName;
 	std::getline(iss, topic);
 
 	std::map< std::string, Channel * >::iterator currentChannel = server.getAllCha().find(channelName);
-	if (currentChannel == server.getAllCha().end()) {
-		log(ERROR, "Channel not found: " + channelName);
-		return (false);
-	}
-	log(DEBUG, "Channel found");
-
 	//does the channel exists
-	if (currentChannel->second == NULL) {
-		sendReply(fd, ERR_NOSUCHCHANNEL(server.getAllCli()[fd]->getNick(), currentChannel->second->getName()));
+	if (currentChannel == server.getAllCha().end()) {
+		sendReply(fd, ERR_NOSUCHCHANNEL(server.getAllCli()[fd]->getNick(), channelName));
 		return (false); }
 
 	std::map<int, Client*>::iterator whatCli = currentChannel->second->getCliInChannel().find(fd);
 	//is the client on the channel
 	if (whatCli == currentChannel->second->getCliInChannel().end()) {
-		sendReply(fd, ERR_NOTONCHANNEL(whatCli->second->getNick(), currentChannel->second->getName()));
+		sendReply(fd, ERR_NOTONCHANNEL(server.getAllCli()[fd]->getNick(), currentChannel->second->getName()));
 		return (false); }
-	log(DEBUG, "Client in channel found");
-
+	
 	//if no params (= topic is empty) after channelname, client only checks the topic
 	if (topic.empty() == true) {
-		if (currentChannel->second->getTopic().empty() == true) {
-			log(DEBUG, "No topic found : ", RPL_NOTOPIC(whatCli->second->getNick(), currentChannel->second->getName()));
-			sendReply(fd, RPL_NOTOPIC(whatCli->second->getNick(), currentChannel->second->getName()));
-			return (true);
-		}
-		log(DEBUG, "Topic found : ", RPL_TOPIC(whatCli->second->getNick(), currentChannel->second->getName(), currentChannel->second->getTopic()));
-		sendReply(fd, RPL_TOPIC(whatCli->second->getNick(), currentChannel->second->getName(), currentChannel->second->getTopic()));
-		return (true);
-	}
+		checkTopic(currentChannel->second, whatCli->second, fd);
+		return (true); }
 
 	//if topic is = ":", the client clears the topic for the channel
 	//sends the notification to all clients of the channel
-	if (topic == ":") {
-		for (std::map<int, Client *>::iterator it = currentChannel->second->getCliInChannel().begin();
-			it != currentChannel->second->getCliInChannel().end(); ++it) {
-				log(DEBUG, "Topic cleared : ", RPL_TOPICCLEARED(whatCli->second->getPrefix(), currentChannel->second->getName()));
-				sendReply(it->second->getFd(), RPL_TOPICCLEARED(whatCli->second->getPrefix(), currentChannel->second->getName())); }
-		return (true);
-	}
-
+	if (topic == " ::") {
+		clearTopic(currentChannel->second, whatCli->second);
+		return (true); }
+	
 	//if topic is :[other_topic], client changes the topic of the channel
 	if (topic.empty() == false) {
-		topic.erase(1, 0); //remove the ':'
-		currentChannel->second->getTopic().clear();
-		currentChannel->second->setTopic(topic);
-		log(DEBUG, "Topic set : ", RPL_TOPICCHANGED(whatCli->second->getPrefix(), currentChannel->second->getName(), currentChannel->second->getTopic()));
+		changeTopic(currentChannel->second, whatCli->second, topic);
+		return (true); }
 
-		for (std::map<int, Client *>::iterator it = currentChannel->second->getCliInChannel().begin();
-			it != currentChannel->second->getCliInChannel().end(); ++it) {
-				sendReply(it->second->getFd(), RPL_TOPICCHANGED(whatCli->second->getPrefix(), currentChannel->second->getName(), currentChannel->second->getTopic())); }
-		return (true);
-	}
-	log(DEBUG, "command Topic failed unexpectedly");
 	return (false);
 }
