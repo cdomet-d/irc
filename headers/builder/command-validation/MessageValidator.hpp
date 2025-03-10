8fc25081243e24763b6354a0d154707f8b8952c8
Coralie Domet De Mont <cdomet-d@z1r3p2.42lyon.fr>
2025-03-10 10:23:04 +0100

    chore: updated function prototypes

--- headers/builder/command-validation/MessageValidator.hpp
+++ headers/builder/command-validation/MessageValidator.hpp
@@ -6,13 +6,14 @@
 /*   By: cdomet-d <cdomet-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 15:45:04 by cdomet-d          #+#    #+#             */
-/*   Updated: 2025/03/07 11:53:27 by cdomet-d         ###   ########.fr       */
+/*   Updated: 2025/03/07 14:23:23 by cdomet-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MESSAGEVALIDATOR_HPP
 #define MESSAGEVALIDATOR_HPP
 
+#include "Client.hpp"
 #include "typedef.hpp"
 #include <string>
 #include <vector>
@@ -26,14 +27,15 @@ class MessageValidator {
 	MessageValidator &operator=(const MessageValidator &rhs);
 
 	/*                               METHODS                                  */
-	static void assess(std::string)
+	static void assess(const Client &sender);
 	/*                               GETTERS                                  */
 
 	/*                               SETTERS                                  */
 
   private:
 	/*                               METHODS                                  */
-	static bool hasPrefix(const std::string &mess);
+	static bool hasPrefix(const std::string &mess,
+						  const std::string &cliPrefix);
 	static bool hasTrailing(const std::string &mess);
 	static bool lenIsValid(const std::string &mess);
 	static std::string getTrailing(const std::string &mess);
