c340094e0fba9b61a7c8bc53e0484ae90d48e675
cha202 <116841786+cha202@users.noreply.github.com>
2025-03-10 20:58:05 +0100

    add: Checkers.hpp

---
+++ headers/builder/command-validation/Checkers.hpp
@@ -0,0 +1,24 @@
+//HEADER
+
+#ifndef CHECKERS_HPP
+# define CHECKERS_HPP
+
+# include "CommandSpec.hpp"
+
+class CommandSpec;
+
+int	pwMatch(CommandSpec& cmd);
+int	isRegistered(CommandSpec& cmd);
+int	validNick(CommandSpec& cmd);
+int	validUser(CommandSpec& cmd);
+int	validChan(CommandSpec& cmd);
+int	joinChanRequest(CommandSpec& cmd);
+int	validTarget(CommandSpec& cmd);
+int	validInvite(CommandSpec& cmd);
+int	OnChan(CommandSpec& cmd);
+int	hasChanPriv(CommandSpec& cmd);
+int	validKick(CommandSpec& cmd);
+int	validMode(CommandSpec& cmd);
+int	validMess(CommandSpec& cmd);
+
+#endif
