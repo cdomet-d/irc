322f3b58fa9b9eb3ccaeed4df6fa53408d01183b
Coralie Domet De Mont <cdomet-d@z1r3p2.42lyon.fr>
2025-03-12 17:01:36 +0100

    feat: in typedef.hpp, replaced #defines with typedefs
    - names remain unchanged
    - includes are now a bit more centralized (<map>, <vector>, <string> now
      in typedef)

--- headers/debug/Log.h
+++ headers/debug/Log.h
@@ -6,14 +6,11 @@
 /*   By: cdomet-d <cdomet-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 14:36:08 by aljulien          #+#    #+#             */
-/*   Updated: 2025/03/11 14:11:56 by cdomet-d         ###   ########.fr       */
+/*   Updated: 2025/03/12 16:48:45 by cdomet-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "typedef.hpp"
-#include <ctime>
-#include <iostream>
-#include <string>
 
 //TODO : should I remove this file ?
 enum logLevel { INFO, ERROR, DEBUG };
