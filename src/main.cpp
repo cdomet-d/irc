ede7e26a45123d4454afe8e1abac4f92df46f6a6
Coralie Domet De Mont <cdomet-d@z1r3p2.42lyon.fr>
2025-03-14 11:50:43 +0100

    add: main from CommandExec branch

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cdomet-d <cdomet-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aljulien <aljulien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 11:11:56 by aljulien          #+#    #+#             */
-/*   Updated: 2025/03/13 16:45:07 by cdomet-d         ###   ########.fr       */
+/*   Updated: 2025/03/11 13:40:55 by aljulien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
-#include "Client.hpp"
-#include "CmdManager.hpp"
-#include <iostream>
 #include <csignal>
 #include <cstdlib>
 
@@ -21,32 +18,34 @@ int gSign = false;
 int gPort = 0;
 std::string gPassword = "";
 
-void SignalHandler(int signum)
-{
+void SignalHandler(int signum) {
 	(void)signum;
 	gSign = true;
 }
 
+int main(int ac, char **av) {
+	(void)av;
+	if (ac != 3)
+		return (std::cout << "Missing arguments (port and password)\n", 0);
 
-int main(int argc, char **argv) {
-	Client client;
-	std::vector< std::string > buffer;
-	CmdManager cmManager;
+	signal(SIGINT, SignalHandler);
+	signal(SIGQUIT, SignalHandler);
 
-	if (argc < 2)
-		return (1);
-	for (int i = 2; i < argc; i++) {
-		buffer.push_back(argv[i]);
-	}
-	client.cliInfo.setRegistration(3);
+	gPort = atoi(av[1]);
+	gPassword = av[2];
+	static Server &server = Server::GetServerInstance(gPort, gPassword);
+	server.servInit();
+	server.servRun();
 
-	//create the map with all registered commands
-	cmManager.generateCmds();
-
-	//execute command
-	try {
-		cmManager.executeCm(cmManager.getCmd(argv[1]).process(buffer, client));
-	} catch (const CmdManager::CmdNotFoundException &e) {
-		std::cout << ERR_UNKNOWNCOMMAND(client.cliInfo.getNick(), argv[1]);
-	}
+/* 	for (channelMapIt it =
+			 server.getAllChan().begin();
+		 it != server.getAllChan().end(); ++it) {
+		std::cout << "Channel: " << it->second->getName() << std::endl;
+		for (clientMapIt itCli =
+				 it->second->getCliInChan().begin();
+			 itCli != it->second->getCliInChan().end(); ++itCli) {
+			std::cout << "Client: " << itCli->second->getNick() << std::endl;
+		}
+	} */
+	return (0);
 }
