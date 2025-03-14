b8ff9ae0beefb138009d4423a7832dca19dfca85
Coralie Domet De Mont <cdomet-d@z1r3p2.42lyon.fr>
2025-03-14 16:30:07 +0100

    fix: empty spaces in cmdParam vector<string>

--- headers/builder/command-validation/MessageValidator.hpp
+++ headers/builder/command-validation/MessageValidator.hpp
@@ -6,7 +6,7 @@
 /*   By: cdomet-d <cdomet-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 15:45:04 by cdomet-d          #+#    #+#             */
-/*   Updated: 2025/03/13 15:05:19 by cdomet-d         ###   ########.fr       */
+/*   Updated: 2025/03/14 14:49:59 by cdomet-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ class MessageValidator {
 	static bool hasPrefix(std::string &mess, const std::string &cliPrefix);
 	static bool hasTrailing(std::string &mess, std::string &trailing);
 	static bool lenIsValid(const std::string &mess, const Client &sender);
-	static void formatMode(stringVec &mode);
+	static void formatMode(Client &sender);
 	static std::string removeNewlines(const std::string &input);
-	static void printCmdParam(const stringVec &obj);
+	static void printCmdParam(const stringVec &obj, std::string where);
 };
 
 #endif
