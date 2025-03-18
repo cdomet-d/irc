7b15019cbd6d086761e16bd893ac59b82f619157
cha202 <116841786+cha202@users.noreply.github.com>
2025-03-18 11:51:40 +0100

    fix: fixed return values

--- src/builder/command-validation/Checkers.cpp
+++ src/builder/command-validation/Checkers.cpp
@@ -13,108 +13,119 @@
 #include "Checkers.hpp"
 
 bool pwMatch(CmdSpec &cmd) {
-	// if (cmd[password][0] != cmd.server_.) //TODO: add un getPass in server.hpp
-	return (0);
+	if (cmd[password_][0] != cmd.server_.getPass()) {
+		// sendReply(cmd.getSender().getFd(),
+		// 		  ERR_PASSWDMISMATCH(cmd.getSender().cliInfo.getNick()));
+		std::cout << ERR_PASSWDMISMATCH(cmd.getSender().cliInfo.getNick());
+		return (false);
+	}
+	return (true);
 }
 
 bool isRegistered(CmdSpec &cmd) {
+	//TODO : si PASS a deja ete fait mais que la registration n'est pas fini que faire ?
 	if (cmd.getSender().cliInfo.getRegistration() == 3) {
-		sendReply(cmd.getSender().getFd(),
-				  ERR_ALREADYREGISTRED(cmd.getSender().cliInfo.getNick()));
-		return (1);
+		// sendReply(cmd.getSender().getFd(),
+		// 		  ERR_ALREADYREGISTRED(cmd.getSender().cliInfo.getNick()));
+		std::cout << ERR_ALREADYREGISTRED(cmd.getSender().cliInfo.getNick());
+		return (false);
 	}
-	return (0);
+	return (true);
 }
 
 bool validNick(CmdSpec &cmd) {
 	(void)cmd;
-	return (0);
+	return (true);
 }
 
 bool validUser(CmdSpec &cmd) {
 	(void)cmd;
-	return (0);
+	return (true);
 }
 
 bool validChan(CmdSpec &cmd) {
 	(void)cmd;
-	return (0);
+	return (true);
 }
 
 bool joinChanRequest(CmdSpec &cmd) {
-	for (size_t i = 0; i < cmd[channel].getSize(); i++)
-		if (cmd[channel][i][0] != '#') {
-			sendReply(cmd.getSender().getFd(),
-					  ERR_NOSUCHCHANNEL(cmd.getSender().cliInfo.getNick(),
-										cmd[channel][i]));
+	for (size_t i = 0; i < cmd[channel_].getSize(); i++)
+		if (cmd[channel_][i][0] != '#') {
+			// sendReply(cmd.getSender().getFd(),
+			// 		  ERR_NOSUCHCHANNEL(cmd.getSender().cliInfo.getNick(),
+			// cmd[channel_][i]));
 		}
 	//supprimer chaque channel faux, (ainsi que toutes les keys ? peut etre pas necessaire)
 	//pour qu'il reste que les channel valide a join pour l'exec
 	//s'ils ont tous ete supprimes mettre valid a false
-	return (0);
+	return (true);
 }
 
 bool validTarget(CmdSpec &cmd) {
 	(void)cmd;
-	return (0);
+	return (true);
 }
 
 bool validInvite(CmdSpec &cmd) {
 	(void)cmd;
-	return (0);
+	return (true);
 }
 
 bool onChan(CmdSpec &cmd) {
 	channelMap::iterator itChan;
 	clientMap::iterator itCl;
 
-	itChan = cmd.server_.getAllChan().find(cmd[channel][0]);
+	itChan = cmd.server_.getAllChan().find(cmd[channel_][0]);
 	Channel chan = *itChan->second;
 	itCl = chan.getCliInChan().find(cmd.getSender().getFd());
 	if (itCl == chan.getCliInChan().end()) {
-		sendReply(cmd.getSender().getFd(),
-				  ERR_NOTONCHANNEL(cmd.getSender().cliInfo.getNick(),
-								   chan.getName()));
-		return (1);
+		// sendReply(cmd.getSender().getFd(),
+		// 		  ERR_NOTONCHANNEL(cmd.getSender().cliInfo.getNick(),
+		//    chan.getName()));
+		std::cout << ERR_NOTONCHANNEL(cmd.getSender().cliInfo.getNick(),
+									  chan.getName());
+		return (false);
 	}
-	return (0);
+	return (true);
 }
 
 bool hasChanPriv(CmdSpec &cmd) {
 	channelMap::iterator itChan;
 
-	itChan = cmd.server_.getAllChan().find(cmd[channel][0]);
+	itChan = cmd.server_.getAllChan().find(cmd[channel_][0]);
 	Channel chan = *itChan->second;
 
 	if (cmd.getName() == "TOPIC" &&
 		(!cmd[topic_].getInnerParam().empty() ||
 		 (chan.getModes().find('t') == std::string::npos))) {
-		return (0);
+		return (true);
 	}
 
 	clientMap::iterator itCl;
 
 	itCl = chan.getOpCli().find(cmd.getSender().getFd());
 	if (itCl == chan.getOpCli().end()) {
-		sendReply(cmd.getSender().getFd(),
-				  ERR_CHANOPRIVSNEEDED(cmd.getSender().cliInfo.getNick(),
-									   chan.getName()));
-		return (1);
+		// sendReply(cmd.getSender().getFd(),
+		// 		  ERR_CHANOPRIVSNEEDED(cmd.getSender().cliInfo.getNick(),
+		//    chan.getName()));
+		std::cout << ERR_CHANOPRIVSNEEDED(cmd.getSender().cliInfo.getNick(),
+										  chan.getName());
+		return (false);
 	}
-	return (0);
+	return (true);
 }
 
 bool validKick(CmdSpec &cmd) {
 	(void)cmd;
-	return (0);
+	return (true);
 }
 
 bool validMode(CmdSpec &cmd) {
 	(void)cmd;
-	return (0);
+	return (true);
 }
 
 bool validMess(CmdSpec &cmd) {
 	(void)cmd;
-	return (0);
+	return (true);
 }
