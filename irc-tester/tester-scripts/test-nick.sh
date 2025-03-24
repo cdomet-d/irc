156fb0aa108e1c69eef80b40e8ecb328778c2561
Charlotte Sweeting <charlottesweeting@outlook.fr>
2025-03-24 20:03:16 +0100

    add: more tests

--- irc-tester/tester-scripts/test-nick.sh
+++ irc-tester/tester-scripts/test-nick.sh
@@ -18,15 +18,31 @@ NICK {cha
 NICK }cha
 NICK +cha
 NICK cha$
-NICK cha-[]\^{}4\`
+NICK cha-[]\^4\`{}
 USER c c c c
 NICK chacham sweet
 NICK cha#
 NICK cha:
+NICK :
+JOIN #chan
+NICK sweet
 EOF
 
-timeout 2s nc 0.0.0.0 4444 < test_input.txt > output.txt
+cat << EOF > client2_input.txt
+PASS 0
+NICK chacham
+NICK bobby
+USER b b b b
+JOIN #chan
+EOF
 
-#TODO: faire un test avec deux client pour essayer d'attribuer un nickname deja pris
-#TODO: tester sur libera chat "NICK :"
+timeout 2s nc 0.0.0.0 4444 < test_input.txt > output.txt & PID1=$!
+
+timeout 2s nc 0.0.0.0 4444 < client2_input.txt > client2_output.txt & PID2=$!
+
+wait $PID1
+wait $PID2
+
+cat client2_output.txt >> output.txt
+
 #TODO: mettre le client dans un channel, changer son nickname et voir si le message est envoye a tout le channel
