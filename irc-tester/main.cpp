7a2e7a5d2887ca8429dbc92e6c19d82bbfd399ee
Charlotte Sweeting <charlottesweeting@outlook.fr>
2025-03-24 11:27:44 +0100

    add: irc-tester

---
+++ irc-tester/main.cpp
@@ -0,0 +1,54 @@
+//HEADER
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sstream>
+
+std::string trim_crlf(std::string& line) {
+    if (!line.empty() && line[line.size() - 1] == '\n') line.erase(line.size() - 1, line.size());
+    if (!line.empty() && line[line.size() - 1] == '\r') line.erase(line.size() - 1, line.size());
+    return line;
+}
+
+int main() {
+    int status = system("./tester.sh");
+
+    if (status == -1) {
+        perror("Erreur lors de l'exécution du script");
+        return (1);
+    }
+
+    std::ifstream output;
+    std::ifstream expectedOutput;
+
+    output.open("output.txt");
+    expectedOutput.open("expected_output.tx");
+    if (!expectedOutput.is_open() || !output.is_open())
+	{
+		std::cout << "Error: could not open file" << std::endl;
+		return (1);
+	}
+    
+    std::string expectedOutputContent;
+    std::string outputContent;
+    size_t      i = 1;
+    std::cout << std::endl;
+    while (getline(expectedOutput, expectedOutputContent, '\n') && getline(output, outputContent, '\n')) {
+        outputContent = trim_crlf(outputContent);
+        expectedOutputContent = trim_crlf(expectedOutputContent);
+
+        if (expectedOutputContent != outputContent) {
+             std::cerr << "\033[31mError on Test " << i << ":\033[0m\n"
+                    << "Expected: " << expectedOutputContent << std::endl
+                    << "Got: " << outputContent << std::endl;
+        }
+        else
+            std::cout << "\033[32mTest " << i << ":\nSuccess\033[0m\n";
+        std::cout << std::endl;
+        i++;
+    }
+    expectedOutput.close();
+    output.close();
+    return (0);
+}
--- irc-tester/tester.sh
+++ irc-tester/tester.sh
@@ -16,32 +16,32 @@ USER c c c c
 EOF
 
 # Envoi des commandes à l'IRC server et capture de la réponse
-nc 0.0.0.0 4444 < test_input.txt > output.txt
+timeout 2s nc 0.0.0.0 4444 < test_input.txt > output.txt
 
 
 # Préparation des fichiers de commandes pour les deux clients
-cat <<EOF > client1_input.txt
-USER user1 u u u
-NICK alpha
-PASS 1234
-EOF
-
-cat <<EOF > client2_input.txt
-USER user2 u u u
-NICK beta
-PASS 5678
-EOF
+#cat <<EOF > client1_input.txt
+#USER user1 u u u
+#NICK alpha
+#PASS 1234
+#EOF
+#
+#cat <<EOF > client2_input.txt
+#USER user2 u u u
+#NICK beta
+#PASS 5678
+#EOF
 
 # Lancement des deux clients en parallèle
 # Chaque client écrit sa sortie dans un fichier différent
 
-nc 0.0.0.0 4444 < client1_input.txt > client1_output.txt &
-PID1=$!
-
-nc 0.0.0.0 4444 < client2_input.txt > client2_output.txt &
-PID2=$!
+#nc 0.0.0.0 4444 < client1_input.txt > client1_output.txt &
+#PID1=$!
+#
+#nc 0.0.0.0 4444 < client2_input.txt > client2_output.txt &
+#PID2=$!
 
 # Attente que les deux clients aient terminé
-wait $PID1
-wait $PID2
+#wait $PID1
+#wait $PID2
 
