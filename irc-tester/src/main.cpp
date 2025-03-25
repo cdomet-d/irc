9a5076c182a443bdcad22e1df892fcf463930bbc
Charlotte Sweeting <charlottesweeting@outlook.fr>
2025-03-25 16:52:38 +0100

    add: test invite

--- irc-tester/src/main.cpp
+++ irc-tester/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 15:40:59 by csweetin          #+#    #+#             */
-/*   Updated: 2025/03/24 16:33:00 by csweetin         ###   ########.fr       */
+/*   Updated: 2025/03/25 16:51:46 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,9 @@ int main() {
 	//NICK
 	if (test("tester-scripts/test-nick.sh", "expected-outputs/nick_output.tx", "NICK:"))
 		return (1);
+	//INVITE
+	// if (test("tester-scripts/test-invite.sh", "expected-outputs/invite_output.tx", "INVITE:"))
+	// 	return (1);
+	
 	return (0);
 }
--- irc-tester/tester-scripts/test-invite.sh
+++ irc-tester/tester-scripts/test-invite.sh
@@ -6,53 +6,40 @@ rm -f client*
 # Création des pipes nommés
 mkfifo client1_in client1_out
 mkfifo client2_in client2_out
+mkfifo client3_in client3_out
 
 # Ouvrir les FIFOs avec des descripteurs de fichiers dynamiques
 exec {client1_in_fd}<>client1_in
 exec {client1_out_fd}<>client1_out
 exec {client2_in_fd}<>client2_in
 exec {client2_out_fd}<>client2_out
+exec {client3_in_fd}<>client3_in
+exec {client3_out_fd}<>client3_out
 
 # Lancer les clients (en lecture/écriture FIFO)
 nc 0.0.0.0 4444 0<&${client1_in_fd} 1>&${client1_out_fd} &
 PID1=$!
 nc 0.0.0.0 4444 0<&${client2_in_fd} 1>&${client2_out_fd} &
 PID2=$!
+nc 0.0.0.0 4444 0<&${client3_in_fd} 1>&${client3_out_fd} &
+PID3=$!
 
 sleep 0.2  # Laisser le temps aux connexions de se faire
 
-# ➤ Envoyer les lignes à client1
+# ➤ client1
 cat <<EOF >&${client1_in_fd}
 PASS 0
-NICK
-NICK :charlotte sweeting
-NICK #cha
-NICK ::cha
-NICK 4cha
-NICK -cha
-NICK [cha
-NICK ]cha
-NICK \\cha
-NICK \`cha
-NICK ^cha
-NICK {cha
-NICK }cha
-NICK +cha
-NICK cha$
-NICK cha-[]\\^4\`{}
+NICK chacham
 USER c c c c
-NICK chacham sweet
-NICK cha#
-NICK cha:
-NICK :
+JOIN #chan
+MODE #chan +i
 EOF
 
 sleep 0.5
 
-# ➤ Envoyer à client2
+# ➤ client2
 cat <<EOF >&${client2_in_fd}
 PASS 0
-NICK chacham
 NICK bobby
 USER b b b b
 JOIN #chan
@@ -60,28 +47,67 @@ EOF
 
 sleep 0.5
 
-# ➤ Join pour les deux clients
-echo "JOIN #chan" >&${client1_in_fd}
-echo "JOIN #chan" >&${client2_in_fd}
+# ➤ client3
+cat <<EOF >&${client3_in_fd}
+PASS 0
+NICK juju
+USER j j j j
+EOF
 
 sleep 0.5
 
-# ➤ client1 change de nick
-echo "NICK sweet" >&${client1_in_fd}
+# ➤ client1
+cat <<EOF >&${client1_in_fd}
+INVITE
+INVITE dontexist #chan
+INVITE bobby #dontexist
+INVITE bobby wrongchan
+INVITE bobby #chan
+EOF
+
+sleep 0.5
+
+# ➤ client2
+cat <<EOF >&${client2_in_fd}
+JOIN #chan
+INVITE juju #chan
+EOF
+
+sleep 0.5
+
+# ➤ client1
+cat <<EOF >&${client1_in_fd}
+INVITE bobby #chan
+MODE #chan -i
+EOF
+
+sleep 0.5
+
+# ➤ client2
+cat <<EOF >&${client2_in_fd}
+INVITE juju #chan
+PART #chan
+INVITE juju #chan
+EOF
+
+sleep 0.5
 
 # Quit pour les deux clients
 echo "QUIT" >&${client1_in_fd}
 echo "QUIT" >&${client2_in_fd}
+echo "QUIT" >&${client3_in_fd}
 
 sleep 3  # Laisser le serveur envoyer les réponses
 
 # Arrêter les processus nc
 kill $PID1 2>/dev/null
 kill $PID2 2>/dev/null
+kill $PID3 2>/dev/null
 
 # Attendre que les processus se terminent
 wait $PID1 2>/dev/null
 wait $PID2 2>/dev/null
+wait $PID3 2>/dev/null
 
 # Récupérer les logs depuis les FIFOs vers un fichier de sortie
 cat client1_out > output.txt &
@@ -92,5 +118,9 @@ exec {client1_in_fd}>&-
 exec {client1_out_fd}>&-
 exec {client2_in_fd}>&-
 exec {client2_out_fd}>&-
+exec {client3_in_fd}>&-
+exec {client3_out_fd}>&-
 
 rm -f client*
+
+trap cleanup EXIT
