13899a4e6b4f34f243ec4d6056c56cdb5f12527e
Charlotte Sweeting <charlottesweeting@outlook.fr>
2025-03-26 12:03:00 +0100

    fix: expected output of user and pass

--- irc-tester/src/main.cpp
+++ irc-tester/src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: charlotte <charlotte@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 15:40:59 by csweetin          #+#    #+#             */
-/*   Updated: 2025/03/25 17:44:19 by csweetin         ###   ########.fr       */
+/*   Updated: 2025/03/26 10:13:36 by charlotte        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int main() {
 	// if (test("tester-scripts/test-invite.sh", "expected-outputs/invite_output.tx", "INVITE:"))
 	// 	return (1);
 	//JOIN
-	if (test("tester-scripts/test-join.sh", "expected-outputs/join_output.tx", "JOIN:"))
-		return (1);
+	// if (test("tester-scripts/test-join.sh", "expected-outputs/join_output.tx", "JOIN:"))
+		// return (1);
 	return (0);
 }
