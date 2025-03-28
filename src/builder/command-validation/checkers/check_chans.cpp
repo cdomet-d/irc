718bab38572c4a63b5bf3cd1d8c190352b91c138
Charlotte Sweeting <charlottesweeting@outlook.fr>
2025-03-28 18:05:33 +0100

    fix: isOnChan, now loops on all channels

--- src/builder/command-validation/checkers/check_chans.cpp
+++ src/builder/command-validation/checkers/check_chans.cpp
@@ -3,26 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   check_chans.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cdomet-d <cdomet-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: charlotte <charlotte@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 13:03:05 by cdomet-d          #+#    #+#             */
-/*   Updated: 2025/03/28 12:59:59 by cdomet-d         ###   ########.fr       */
+/*   Updated: 2025/03/28 17:43:35 by charlotte        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "validator.hpp"
 
-bool check::chans_::isOnChan(CmdSpec &cmd) {
-	const stringVec &joinedChans = cmd.getSender().getJoinedChans();
-	for (size_t i = 0; i < joinedChans.size(); i++) {
-		if (joinedChans[i] == cmd[channel_][0])
+bool findString(stringVec array, std::string& strToFind) {
+	for (size_t i = 0; i < array.size(); i++) {
+		if (array[i] == strToFind)
 			return (true);
 	}
-	if (cmd.getName() != "JOIN")
-		reply::send_(cmd.getSender().getFd(),
-					ERR_NOTONCHANNEL(cmd.getSender().cliInfo.getNick(),
-									 cmd[channel_][0]));
 	return (false);
+}
+
+bool check::chans_::isOnChan(CmdSpec &cmd) {
+	stringVec joinedChans = cmd.getSender().getJoinedChans();
+
+	size_t i = 0;
+	while (i < cmd[channel_].size()) {
+		if (findString(joinedChans, cmd[channel_][i]))
+			i++;
+		else {
+			reply::send_(cmd.getSender().getFd(),
+						 ERR_NOTONCHANNEL(cmd.getSender().cliInfo.getNick(),
+										  cmd[channel_][i]));
+			cmd[channel_].rmParam(i);
+		}
+	}
+	if (!cmd[channel_].size())
+		return (false);
+	return (true);
 }
 
 bool check::chans_::hasChanAuthorisations(CmdSpec &cmd) {
@@ -42,8 +56,8 @@ bool check::chans_::hasChanAuthorisations(CmdSpec &cmd) {
 	itCl = chan.getOpCli().find(cmd.getSender().getFd());
 	if (itCl == chan.getOpCli().end()) {
 		reply::send_(cmd.getSender().getFd(),
-					ERR_CHANOPRIVSNEEDED(cmd.getSender().cliInfo.getNick(),
-										 chan.getName()));
+					 ERR_CHANOPRIVSNEEDED(cmd.getSender().cliInfo.getNick(),
+										  chan.getName()));
 		return (false);
 	}
 	return (true);
