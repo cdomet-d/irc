c85573414b67a4114c4216d88048a6347a8c4a59
Charlotte Sweeting <charlottesweeting@outlook.fr>
2025-04-10 16:04:17 +0200

    chore: add ":" in privmsg reply

--- headers/server/Reply.hpp
+++ headers/server/Reply.hpp
@@ -95,7 +95,7 @@
 #define RPL_KICK(prefix, channel, target, reason) (":" + prefix + " KICK " + channel + " " + target + " " + reason + "\r\n")
 #define RPL_PARTNOREASON(prefix, channel) (":" + prefix + " PART " + channel + "\r\n")
 #define RPL_PARTREASON(prefix, channel, reason) (":" + prefix + " PART " + channel + " " + reason + "\r\n")
-#define RPL_PRIVMSG(prefix, target, message) (":" + prefix + " PRIVMSG " + target + " " + message + "\r\n")
+#define RPL_PRIVMSG(prefix, target, message) (":" + prefix + " PRIVMSG " + target + " :" + message + "\r\n")
 #define RPL_TOPICCHANGED(prefix, channel, Topic) (":" + prefix + " TOPIC " + channel + " :" + Topic + "\r\n")
 #define RPL_MODE(prefix, channel, Mode, modeArgs) (":" + prefix + " MODE " + channel + " " + Mode + " " + modeArgs + "\r\n")
 #define RPL_QUIT(prefix, message) (":" + prefix + " :Client Quit " + message + "\r\n")
