b04bbc186af7c7f52d7e6cc4e3c36a8bfa887c13
Charlotte Sweeting <charlottesweeting@outlook.fr>
2025-04-10 12:21:00 +0200

    fix: used RPL_WHOREPLY instead of RPL_NAMEREPLY

--- src/builder/command-execution/Who.cpp
+++ src/builder/command-execution/Who.cpp
@@ -15,41 +15,35 @@
 #include "Server.hpp"
 #include <sstream>
 
-std::string buildNickList(clientMap curMap, Client *sender, Channel &curChan) {
-	std::string list;
-	(void)sender;
-	for (clientMapIt it = curMap.begin(); it != curMap.end(); ++it) {
-		// if (it->first == sender->getFd())
-		// 	continue;
-		std::string prefix = "";
-		if (curChan.getOpCli().find(it->first) != curChan.getOpCli().end()) {
-			prefix = "@";
-		}
-		if (!list.empty()) {
-			list += " ";
-		}
-		list += prefix + it->second->cliInfo.getNick();
-	}
-	return (list);
-}
-
-void sendNickList(std::string &nickList, Client *sender, Channel &curChan) {
-
-	reply::send_(sender->getFd(), RPL_NAMREPLY(sender->cliInfo.getNick(), "=",
-											   curChan.getName(), nickList));
-	reply::send_(sender->getFd(),
-				 RPL_ENDOFNAMES(sender->cliInfo.getNick(), curChan.getName()));
-}
-
 void who(CmdSpec &cmd) {
-	Client *sender = &cmd.getSender();
 	Channel &curChan = findCurChan(cmd[channel_][0]);
 	std::string nickList;
 
-	if (!cmd[flag_].empty() && cmd[flag_][0] == "o")
-		nickList = buildNickList(curChan.getOpCli(), sender, curChan);
-	else
-		nickList = buildNickList(curChan.getCliInChan(), sender, curChan);
-	sendNickList(nickList, sender, curChan);
-	//TODO: use numeric reply RPL_WHOREPLY and RPL_ENDOFWHO instead of RPL_NAMREPLY and RPL_ENDOFNAMES?
+	if (!cmd[flag_].empty() && cmd[flag_][0] == "o") {
+		for (clientMapIt it = curChan.getOpCli().begin();
+			 it != curChan.getOpCli().end(); ++it) {
+			reply::send_(cmd.getSdFd(),
+						 RPL_WHOREPLY(it->second->cliInfo.getNick(), cmd[channel_][0],
+									  it->second->cliInfo.getUsername(),
+									  it->second->cliInfo.getHostname(), "@",
+									  it->second->cliInfo.getRealName()));
+		}
+	} else {
+		std::string flag;
+		for (clientMapIt it = curChan.getCliInChan().begin();
+			 it != curChan.getCliInChan().end(); ++it) {
+			flag = "";
+			if (curChan.getOpCli().find(it->second->getFd()) !=
+				curChan.getOpCli().end())
+				flag = "@";
+			reply::send_(cmd.getSdFd(),
+						 RPL_WHOREPLY(it->second->cliInfo.getNick(),
+									  cmd[channel_][0],
+									  it->second->cliInfo.getUsername(),
+									  it->second->cliInfo.getHostname(), flag,
+									  it->second->cliInfo.getRealName()));
+		}
+	}
+	reply::send_(cmd.getSdFd(),
+				 RPL_ENDOFWHO(cmd.getSdNick(), cmd[channel_][0]));
 }
