e6178ac118b8a60be86ad24ada91bb0c95e7780c
Charlotte Sweeting <charlottesweeting@outlook.fr>
2025-04-14 21:13:05 +0200

    fix: put NOTICE instead of MODE in ERR_BADKEYLEN for hexchat

--- headers/server/Reply.hpp
+++ headers/server/Reply.hpp
@@ -89,7 +89,7 @@
 #define RPL_BYEYBE(nickname) (":irc.bitchat.net " + nickname + " :You quitted the server, see you next time !\r\n")
 #define RPL_ERROR(host, reason) ("ERROR :Closing Link: " + host + " (" + reason + ")\r\n")
 //command_replies (uses prefix)
-#define ERR_BADKEYLEN(prefix, channel) (":" + prefix + " MODE " + channel + " :Bad key (+k) - key len must be at least 8 and no more than 26\r\n")
+#define ERR_BADKEYLEN(prefix, channel) (":irc.bitchat.net NOTICE " + channel + " :Bad key (+k) - key len must be at least 8 and no more than 26\r\n")
 #define ERR_BADINPUT(prefix, cmd, expected, received) (":" + prefix + " " + cmd + " :format should be [" + expected + "], is [" + received + "]\r\n")
 #define RPL_INVITE(prefix, target, channel) (":" + prefix + " INVITE " + target + " :" + channel + "\r\n")
 #define RPL_JOIN(prefix, channel) (":" + prefix + " JOIN " + channel + "\r\n")
