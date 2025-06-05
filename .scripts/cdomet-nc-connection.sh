7d7f9b98bd4a2999fe335208cd5b02cbfd05b1f7
Coralie Domet De Mont <cdomet-d@z1r3p2.42lyon.fr>
2025-06-05 09:58:55 +0200

    chore: improved srcgen script

--- .scripts/cdomet-nc-connection.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-(
-echo "PASS 0"
-echo "NICK coco"
-echo "USER coco 0 0 :delanight"
-) | nc 0.0.0.0 80
--- .scripts/list-sources.sh
+++ .scripts/list-sources.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-if [ "$(rm src.mk)" == 0 ]; then
+set eu
+
+if [ -s src.mk  ]; then
 	rm src.mk
 fi
 
 prevdir="";
 
-find src -type f -name "*.cpp" | while read -r file; do
+echo -n "Enter a directory to list: "
+
+read -r dir;
+
+find "$dir" -type f -name "*.cpp" | while read -r file; do
 	dir="$(dirname "$file")"
 	if [ "$prevdir" != "$dir" ]; then
 		echo >> src.mk
-		echo "in $dir" >> src.mk
+		echo "$dir" >> src.mk
 		prevdir="$dir";
 	fi
 	filename="$(basename "$file")"
-	echo "	$filename \\" >> src.mk
+	echo "		$filename \\" >> src.mk
 done
